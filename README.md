88f581419084f4598b2de068c042a4b45c77077f
Dave Budhram <davebudhram18@gmail.com>
2023-12-06 15:11:13 -0500

    Fix MongoDB connection issue

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Net Natter
 Net Natter is a place for NBA fans to view past game scores and stats, comment on games. Analysts can post articles relating the NBA.
+
+## Running the Application
+
+### Prerequisites
+- [Node.js](https://nodejs.org/en/)
+
+### Setup
+1. **Clone the repository**
+  ```bash
+   git clone https://github.com/davebudhram/net-natter.git
+   cd net-natter
+   ```
+2. Running the Backend
+  ```bash
+  cd server
+  npm install
+  nodemon app.ts
+  ```
+3. Running the Frontend
+  ```bash
+  cd client
+  npm install
+  npm start
+  ```
--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,22 @@
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose';
+import "dotenv/config";
 
 // Create a new express app instance
 const app = express();
 const port = 4000;
 
 // Connect to MongoDB
-const CONNECTION_STRING: string | undefined = process.env.DB_CONNECTION_STRING as string;
+const CONNECTION_STRING: string = process.env.DB_CONNECTION_STRING || '';
 if (!CONNECTION_STRING) {
   throw new Error('DB_CONNECTION_STRING is not defined in the environment variables');
 }
-mongoose.connect(CONNECTION_STRING);
+try {
+  mongoose.connect(CONNECTION_STRING,)
+}
+catch (error) {
+  console.log(error);
+}
 
 
 app.get('/hello', (req: Request, res: Response) => {
