765fbad46842291953d8c4c304c65b1e2d859c3a
Dave Budhram <davebudhram18@gmail.com>
2023-12-06 11:02:55 -0500

    Adding connection to database

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,17 @@
 import express, {Request, Response} from 'express';
+import mongoose from 'mongoose';
 
+// Create a new express app instance
 const app = express();
 const port = 4000;
+
+// Connect to MongoDB
+const CONNECTION_STRING: string | undefined = process.env.DB_CONNECTION_STRING as string;
+if (!CONNECTION_STRING) {
+  throw new Error('DB_CONNECTION_STRING is not defined in the environment variables');
+}
+mongoose.connect(CONNECTION_STRING);
+
 
 app.get('/hello', (req: Request, res: Response) => {
     res.send('Hello World!');
