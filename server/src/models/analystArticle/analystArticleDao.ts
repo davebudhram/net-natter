4b4fc0c219d5754865c9680edef594b1bf1ad518
Dave Budhram <davebudhram18@gmail.com>
2023-12-08 22:18:43 -0500

    created simple navbar

--- server/src/models/analystArticle/analystArticleDao.ts
+++ server/src/models/analystArticle/analystArticleDao.ts
@@ -1,7 +1,9 @@
 //analystArticleDao.ts
-import AnalystArticleModel from './analystArticle';
-import { IAnalystArticle, IAnalystArticleDTO } from '../../interfaces/analystArticle';
-
+import AnalystArticleModel from "./analystArticle";
+import {
+  IAnalystArticle,
+  IAnalystArticleDTO,
+} from "../../interfaces/analystArticle";
 
 class AnalystArticleDao {
   static async getAllAnalystArticles(): Promise<IAnalystArticle[]> {
@@ -9,43 +11,58 @@ class AnalystArticleDao {
       const articles = await AnalystArticleModel.find();
       return articles;
     } catch (error) {
-      throw new Error('Error fetching articles from the database');
+      throw new Error("Error fetching articles from the database");
     }
   }
 
-  static async getArticlesByAuthorId(authorId: string): Promise<IAnalystArticle[] | null> {
+  static async getArticlesByAuthorId(
+    authorId: string
+  ): Promise<IAnalystArticle[] | null> {
     try {
-      const articles = await AnalystArticleModel.find({ authorId: authorId }).exec();
+      const articles = await AnalystArticleModel.find({
+        authorId: authorId,
+      }).exec();
       return articles;
     } catch (error) {
-      throw new Error('Error fetching articles from the database');
+      throw new Error("Error fetching articles from the database");
     }
   }
 
-  static async getArticleById(articleId: string): Promise<IAnalystArticle | null> {
+  static async getArticleById(
+    articleId: string
+  ): Promise<IAnalystArticle | null> {
     try {
       const article = await AnalystArticleModel.findById(articleId);
       return article;
     } catch (error) {
-      throw new Error('Error fetching articles from the database');
+      throw new Error("Error fetching articles from the database");
     }
   }
 
-  static async createAnalystArticle(articleData: IAnalystArticleDTO): Promise<IAnalystArticle> {
+  static async createAnalystArticle(
+    articleData: IAnalystArticleDTO
+  ): Promise<IAnalystArticle> {
     try {
       const newArticle = await AnalystArticleModel.create(articleData);
       return newArticle;
     } catch (error) {
-      throw Error('Error creating article in the database');
+      throw Error("Error creating article in the database");
     }
   }
 
-  static async updateAnalystArticle(articleId: string, updatedArticleData: Partial<IAnalystArticleDTO>): Promise<IAnalystArticle | null> {
+  static async updateAnalystArticle(
+    articleId: string,
+    updatedArticleData: Partial<IAnalystArticleDTO>
+  ): Promise<IAnalystArticle | null> {
     try {
-      const updatedArticle = await AnalystArticleModel.findByIdAndUpdate(articleId, updatedArticleData, { new: true });
+      const updatedArticle = await AnalystArticleModel.findByIdAndUpdate(
+        articleId,
+        updatedArticleData,
+        {new: true}
+      );
       return updatedArticle;
     } catch (error) {
-      throw new Error('Error updating article in the database');
+      throw new Error("Error updating article in the database");
     }
   }
 
@@ -53,7 +70,7 @@ class AnalystArticleDao {
     try {
       await AnalystArticleModel.findByIdAndDelete(articleId);
     } catch (error) {
-      throw new Error('Error deleting article from the database');
+      throw new Error("Error deleting article from the database");
     }
   }
 }
