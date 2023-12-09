c9e18cdd30cbb760e0e9b8ff57e2b8854fe0dd16
joey-tsai <joeytsai712@gmail.com>
2023-12-09 10:46:35 -0500

    Changes

--- client/src/server/statsData.tsx
+++ client/src/server/statsData.tsx
@@ -49,7 +49,7 @@ const getPlayerStatsPerGameData = async (gameId: number): Promise<IPlayerStats[]
 
 // Get All Game Stats
 const getGameStatsData = async (gameId: number): Promise<IGameTeamStats[]> => {
-    const params = { "game": gameId }
+    const params = { "id": gameId }
     try {
         const gameStatsRawResponse = await axios.get(`${url}/games/statistics`, {
             params,
@@ -61,25 +61,25 @@ const getGameStatsData = async (gameId: number): Promise<IGameTeamStats[]> => {
             return {
                 gameId: gameId,
                 teamId: rawStats.team.id,
-                points: rawStats.statistics.points,
-                fgm: rawStats.statistics.fgm,
-                fga: rawStats.statistics.fga,
-                fgp: rawStats.statistics.fgp,
-                ftm: rawStats.statistics.ftm,
-                fta: rawStats.statistics.fta,
-                ftp: rawStats.statistics.ftp,
-                tpm: rawStats.statistics.tpm,
-                tpa: rawStats.statistics.tpa,
-                tpp: rawStats.statistics.tpp,
-                offensiveRebounds: rawStats.statistics.offReb,
-                defensiveRebounds: rawStats.statistics.defReb,
-                totalRebounds: rawStats.statistics.totReb,
-                assists: rawStats.statistics.assists,
-                steals: rawStats.statistics.steals,
-                blocks: rawStats.statistics.blocks,
-                fouls: rawStats.statistics.pFouls,
-                plusMinus: rawStats.statistics.plusMinus,
-                turnovers: rawStats.statistics.turnovers,
+                points: rawStats.statistics['0'].points,
+                fgm: rawStats.statistics['0'].fgm,
+                fga: rawStats.statistics['0'].fga,
+                fgp: rawStats.statistics['0'].fgp,
+                ftm: rawStats.statistics['0'].ftm,
+                fta: rawStats.statistics['0'].fta,
+                ftp: rawStats.statistics['0'].ftp,
+                tpm: rawStats.statistics['0'].tpm,
+                tpa: rawStats.statistics['0'].tpa,
+                tpp: rawStats.statistics['0'].tpp,
+                offensiveRebounds: rawStats.statistics['0'].offReb,
+                defensiveRebounds: rawStats.statistics['0'].defReb,
+                totalRebounds: rawStats.statistics['0'].totReb,
+                assists: rawStats.statistics['0'].assists,
+                steals: rawStats.statistics['0'].steals,
+                blocks: rawStats.statistics['0'].blocks,
+                fouls: rawStats.statistics['0'].pFouls,
+                plusMinus: rawStats.statistics['0'].plusMinus,
+                turnovers: rawStats.statistics['0'].turnovers,
             };
         });
         console.log(gameStats);
