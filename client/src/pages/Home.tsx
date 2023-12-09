372123b7de13a5a7408e10aeebf7d10e51dd5a37
joey-tsai <joeytsai712@gmail.com>
2023-12-09 16:45:26 -0500

    finish endpoint cals

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { getPlayerStatsPerGameData, getGameStatsData } from "../services/statsData";
-import { getTeamsData } from "../services/teamData";
-import { getGameData } from "../services/gameData";
-import { Status } from "../interfaces/game";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
       <p>This is the home page</p>
-      <button onClick={async () => await getGameStatsData(13776)}> TEST </button>
     </div>
   );
 }
--- client/src/services/gameData.tsx
+++ client/src/services/gameData.tsx
@@ -1,10 +1,55 @@
 import axios from "axios";
-import { url, headers } from "./endpointTypes";
-import { Status, IGame } from "../interfaces/game";
+import { season, url, headers } from "./endpointTypes";
+import { IGame } from "../interfaces/game";
 
-// Get Game Data based on statues 
-const getGameData = async (status: Status): Promise<IGame[]> => {
-    const params = { "live": status }
+// Get All Upcoming Games 
+const getUpcomingGameData = async (): Promise<IGame[]> => {
+    try {
+        const params = { "season": season }
+        const allGames = await parseGameData(params);
+
+        const upcomingGames = allGames.filter((game) => {
+            const gameDate = new Date(game.startTime);
+            const currentDate = new Date();
+            return gameDate > currentDate;
+        });
+
+        console.log(upcomingGames);
+        return upcomingGames;
+    } catch (error) {
+        console.error('Error fetching game stats:', error);
+        throw error;
+    }
+}
+
+// Get All Games Data of a Team given Team ID
+const getIndividualGameTeamData = async (teamId: number): Promise<IGame[]> => {
+    try {
+        const params = { "team": teamId, "season": season }
+        const games = await parseGameData(params);
+        console.log(games);
+        return games;
+    } catch (error) {
+        console.error('Error fetching game stats:', error);
+        throw error;
+    }
+}
+
+// Get Live Game Data 
+const getLiveGameData = async (): Promise<IGame[]> => {
+    // Date Fields for Manipulation
+    try {
+        const params = { "live": "all" }
+        const games = await parseGameData(params);
+        console.log(games);
+        return games;
+    } catch (error) {
+        console.error('Error fetching game stats:', error);
+        throw error;
+    }
+};
+
+const parseGameData = async (params: any): Promise<IGame[]> => {
     try {
         const gameRawResponse = await axios.get(`${url}/games`, {
             params,
@@ -15,7 +60,7 @@ const getGameData = async (status: Status): Promise<IGame[]> => {
         const games: IGame[] = gameRawData.map((rawGame: any) => {
             return {
                 _id: rawGame.id,
-                startTime: new Date(rawGame.date.start),
+                startTime: rawGame.date.start,
                 arena: rawGame.arena.name,
                 homeTeamId: rawGame.teams.home.id,
                 awayTeamId: rawGame.teams.visitors.id,
@@ -28,16 +73,15 @@ const getGameData = async (status: Status): Promise<IGame[]> => {
                 homeLogo: rawGame.teams.home.logo,
                 awayLogo: rawGame.teams.visitors.logo,
                 quarter: rawGame.periods.current, 
-                status: status,
+                status: rawGame.status.long,
                 clock: rawGame.status.clock,
             };
         });
-        console.log(games);
         return games;
     } catch (error) {
-        console.error('Error fetching standings:', error);
+        console.error('Error fetching game stats:', error);
         throw error;
     }
 };
 
-export { getGameData };
+export { getLiveGameData, getIndividualGameTeamData, getUpcomingGameData };
