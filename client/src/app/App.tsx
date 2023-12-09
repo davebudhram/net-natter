238d4df6ec453cb1f1500ec816f7b0eddd210062
Dave Budhram <davebudhram18@gmail.com>
2023-12-09 08:51:28 -0500

    Create seperate app folder

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import AppRouter from "./Router";
+import AppRouter from "../Router";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
