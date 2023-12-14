0a060b6a877e356e9d1bbb28a37f6a7828ae6f32
joey-tsai <joeytsai712@gmail.com>
2023-12-14 09:09:35 -0500

    Small Changes

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,35 +1,9 @@
 import React, {useState} from "react";
-import GameCard from "../../components/gameCards/gameCard";
 import {IGame} from "../../interfaces/game";
-import LiveGameCard from "../../components/gameCards/liveGameCard";
 import TeamCard from "../../components/teamCards/teamCard";
 import "./Home.css";
-
-import {getDateGameData} from "../../services/gameData";
-import {getPlayerStatsPerGameData} from "../../services/statsData";
-import { getPlayersData } from "../../services/playerData";
 import { ITeam } from "../../interfaces/team";
 
-const temp: IGame = {
-  arena: "Smoothie King Center",
-  awayLogo:
-    "https://upload.wikimedia.org/wikipedia/fr/thumb/d/de/Houston_Rockets_logo_2003.png/330px-Houston_Rockets_logo_2003.png",
-  awayTeamCode: "HOU",
-  awayTeamId: 14,
-  awayTeamName: "Houston Rockets",
-  awayTeamScore: 20,
-  clock: "3:18",
-  homeLogo:
-    "https://upload.wikimedia.org/wikipedia/fr/thumb/2/21/New_Orleans_Pelicans.png/200px-New_Orleans_Pelicans.png",
-  homeTeamCode: "NOP",
-  homeTeamId: 23,
-  homeTeamName: "New Orleans Pelicans",
-  homeTeamScore: 33,
-  quarter: 2,
-  startTime: new Date("2023-12-24T00:00:00.000Z"),
-  status: "Scheduled",
-  _id: 12940,
-};
 
 const teamTemp: ITeam = {
   _id: 1,
